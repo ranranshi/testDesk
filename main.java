b82fb1ec30bd590927de134ff8639763667461b8
时然然 <75431539+ranranshi@users.noreply.github.com>
2025-04-28 20:49:07 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,21 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello, World!");
+    }
+
+    public static void greet() 
+    {
+        System.out.println("Hello from the greet method!");
+    }
+
+    public static void farewell() 
+    {
+        System.out.println("Goodbye from the farewell method!");
+    }
+
+    // This method is not used in the main method but can be called separately
+
+    
+}
